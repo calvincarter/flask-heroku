b9f0f172a89d2d9383eddc3efcb85c4e77457b5c
Calvin Carter <calvincarter@pingidentity.com>
2022-02-22 07:50:14 -0600

    add dotenv

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ import os
 from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import db, connect_db, Pet
+from dotenv import load_dotenv
+
+if os.environ.get('FLASK_ENV')=="development":
+    load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
 
-# https://stackoverflow.com/questions/66690321/flask-and-heroku-sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy
-db_url = (os.environ.get('DATABASE_URL', 'postgres:///heroku')).replace("://", "ql://", 1)
 
-app.config['SECRET_KEY'] = (os.environ.get('SECRET_KEY', 'noonewilleverknow'))
+
+# https://stackoverflow.com/questions/66690321/flask-and-heroku-sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy
+db_url = (os.environ.get('DATABASE_URL')).replace("://", "ql://", 1)
+
+app.config['SECRET_KEY'] = (os.environ.get('SECRET_KEY'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
